aa8a1e0fcf24df5d4781424fde6977ca1c6f29e6
Prada <bruno.prada@nanopowersemi.com>
2026-08-06 08:39:07 +0100

    Turn OFF LED of MCU Board

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -49,7 +49,7 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(Host_Led_GPIO_Port, Host_Led_Pin, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(Host_Led_GPIO_Port, Host_Led_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : Host_Led_Pin */
   GPIO_InitStruct.Pin = Host_Led_Pin;
